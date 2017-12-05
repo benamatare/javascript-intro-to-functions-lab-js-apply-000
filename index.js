af0e2062e0eb847ec124d731b79d5175d37ab72d
Ben Matare <ben.a.matare@gmail.com>
2017-12-05 19:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var string = 'Hello'
 var upperCase = 'I LOVE YOU, GRANDMA'
   // upperCase.toUpperCase() === uppercase; True
@@ -31,3 +32,25 @@ function sayHiToGrandma(string){
           return "I love you, too."
       }
     }
+=======
+
+//1 shout(string)
+function shout(string) {
+  return string.toUpperCase()
+};
+//2 whisper(string)
+function whisper(string) {
+  return string.toLowerCase()
+}
+//3 logShout(string)
+
+console.log(string.toUpperCase);
+
+//4 logWhisper(string)
+function logWhisper(string){
+  return console.log(whisper)
+};
+//5 sayHitoGrandma(string)
+//6 sayHitoGrandma(string)
+//7 sayHitoGrandma(string)
+>>>>>>> 1b1d7755cdd24b2cd9ca9e2efad44dd3bcdd0024
